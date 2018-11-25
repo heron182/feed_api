fb67d7af2ab4372d56783910d93ac990cd78af4b
Heron Rossi <heron.rossi@hotmail.com>
2018-11-24 23:15:17 -0200

    Use mongoengine and remove pymongo dependency

--- feed/api.py
+++ feed/api.py
@@ -1,21 +1,16 @@
-from bson.objectid import ObjectId
 from flask import Blueprint, abort, g, jsonify, request
 from flask_httpauth import HTTPBasicAuth
 from passlib.apps import custom_app_context as pwd_context
-from pymongo import MongoClient, ReturnDocument
 
-from .utils import bson_to_dict
+from .models import Comment, Post, User, Author
 
 api = Blueprint("api", __name__, url_prefix="/api")
 auth = HTTPBasicAuth()
 
-mongo_client = MongoClient("db", 27017)
-db = mongo_client.api
-
 
 @auth.verify_password
 def verify_password(username, password):
-    user = db.users.find_one({"email": username})
+    user = User.objects.get(email=username)
     if user and pwd_context.verify(password, user["password"]):
         g.user = user
         return True
@@ -27,25 +22,24 @@ def verify_password(username, password):
 @auth.login_required
 def feed():
     if request.method == "GET":
-        posts = db.feed.find()
-
-        posts = bson_to_dict(posts)
-        return jsonify(posts)
+        return jsonify({"result": Post.objects})
 
     elif request.method == "POST":
         document = {}
 
         try:
-            document["author"] = request.json["author"]
+
+            document["author"] = Author(name=g.user.name, email=g.user.email)
             document["title"] = request.json["title"]
             document["content"] = request.json["content"]
+
+            post = Post.objects.create(**document)
+            return jsonify({"result": post})
 
         except KeyError:
             abort(400)
 
-        db.feed.insert_one(document)
-
-        return jsonify(bson_to_dict(document))
+        return jsonify({"result": post})
 
 
 @api.route("/feed/<feed_id>/comment", methods=["POST"])
@@ -54,31 +48,24 @@ def feed_comment(feed_id):
     document = {}
 
     try:
-        document["author"] = g.user
-        document["author"].pop("password")
+        document["author"] = Author(name=g.user.name, email=g.user.email)
         document["content"] = request.json["content"]
 
     except KeyError:
         abort(400)
 
-    document["_id"] = ObjectId()
-    feed = db.feed.find_one_and_update(
-        {"_id": ObjectId(feed_id)},
-        {"$push": {"comments": document}},
-        return_document=ReturnDocument.AFTER,
-    )
+    comment = Comment(**document)
+    post = Post.objects.get(id=feed_id)
+    post.comments.append(comment)
 
-    return jsonify(bson_to_dict(feed))
+    return jsonify({"result": post}), 201
 
 
 @api.route("/users", methods=["GET"])
 @auth.login_required
 def list_users():
     if request.method == "GET":
-        users = db.users.find()
-
-        users = bson_to_dict(users)
-        return jsonify(users)
+        return jsonify({"result": User.objects}), 200
 
 
 @api.route("/users", methods=["POST"])
@@ -93,6 +80,6 @@ def create_user():
     except KeyError:
         abort(400)
 
-    db.users.insert_one(document)
+    user = User.objects.create(**document)
 
-    return jsonify(bson_to_dict(document))
+    return jsonify({"result": user}), 201
--- feed/models.py
+++ feed/models.py
@@ -1,27 +1,31 @@
 from mongoengine import (
     Document,
     EmailField,
-    ListField,
-    ReferenceField,
-    StringField,
     EmbeddedDocument,
     EmbeddedDocumentField,
+    ListField,
+    StringField,
 )
 
 
 class User(Document):
     name = StringField(required=True, max_length=100)
-    password = StringField(required=True, min_length=8, max_length=50)
+    password = StringField(required=True)
+    email = EmailField(required=True)
+
+
+class Author(EmbeddedDocument):
+    name = StringField(required=True, max_length=100)
     email = EmailField(required=True)
 
 
 class Comment(EmbeddedDocument):
-    author = ReferenceField(User)
-    comment = StringField(required=True, max_length=200)
+    author = EmbeddedDocumentField(Author)
+    content = StringField(required=True, max_length=200)
 
 
 class Post(Document):
-    author = ReferenceField(User)
+    author = EmbeddedDocumentField(Author)
     title = StringField(required=True, max_length=100)
     content = StringField(required=True)
     comments = ListField(EmbeddedDocumentField(Comment))
